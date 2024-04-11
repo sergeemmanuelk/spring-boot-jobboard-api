63cde194f3460f3c6942f6ba278544c11984ff81
sergeemmanuelk <serge_em@outlook.fr>
2024-04-11 22:16:29 +0000

    initial commit

---
+++ src/test/java/dev/skonan/jobboard/JobboardApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.skonan.jobboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
